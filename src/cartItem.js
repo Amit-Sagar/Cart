69c84917f90ff5b3348040da0122f0805025ae1a
Amit-Sagar <s.amitsagar99@gmail.com>
2021-06-21 14:12:49 +0530

    adding action-item to the cart

--- src/cartItem.js
+++ src/cartItem.js
@@ -2,6 +2,16 @@ import { getSuggestedQuery } from '@testing-library/react';
 import React from 'react';
 
 class CartItem extends React.Component{
+
+    constructor (){
+        super();
+        this.state= {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render () {
         return (
            <div className="cart-item">
@@ -9,11 +19,14 @@ class CartItem extends React.Component{
                     <img style= {styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 45} } >Phone</div>
-                    <div style={ {fontSize:25,color:'grey' } }>Rs 999</div>
-                    <div style={ {fontSize:25, color: 'grey'} }>1</div>
+                    <div style={ {fontSize: 55} } >{this.state.title}</div>
+                    <div style={ {fontSize:35,color:'grey' } }>Rs {this.state.price}</div>
+                    <div style={ {fontSize:25, color: 'grey'} }>Qty: {this.state.qty}</div>
                     <div className="cart-item-action">
                         {/* Button */}
+                        <img alt="increase" className="action-item" src="https://image.flaticon.com/icons/png/128/149/149145.png" />
+                        <img alt="decrease" className="action-item" src="https://image.flaticon.com/icons/png/128/814/814039.png" />
+                        <img alt="delete" className="action-item" src="https://image.flaticon.com/icons/png/128/1214/1214428.png" />
                     </div>
                 </div>
            </div>
