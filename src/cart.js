185fe4fc9fe327e9adac3167495664b8f2e92019
Amit-Sagar <s.amitsagar99@gmail.com>
2021-06-22 17:17:16 +0530

    adding cart component

---
+++ src/cart.js
@@ -0,0 +1,19 @@
+import { getSuggestedQuery } from '@testing-library/react';
+import React from 'react';
+import CartItem from './cartItem';
+
+class Cart extends React.Component{
+render() {
+    return (
+           <div className="cart">
+               <CartItem />
+               <CartItem />
+               <CartItem />
+
+           </div>  
+            );
+         }
+}  
+    
+
+export default Cart;
